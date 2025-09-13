12be5b7ad827b4b7bfa6d7e2fd553753c9aae042
Rajan Singh ✅ <145154389+versatilerajan@users.noreply.github.com>
2025-09-14 00:51:06 +0530

    Update Secondpage.tsx

--- components/Secondpage.tsx
+++ components/Secondpage.tsx
@@ -28,8 +28,17 @@ export default function SecondPage() {
           Crafting Awesome Software and Killer ML Model To Make Rajan Stand Out
         </p>
         <div className="cta-buttons">
-          <a href="mailto:rr791337@gmail.com" class="cta-btn primary">Contact Us</a>
-          <a href="https://www.linkedin.com/in/rajan-singh-5a9089295/" className="cta-btn secondary">Connect me</a>
+          <a href="mailto:rr791337@gmail.com" className="cta-btn primary">
+            Contact Us
+          </a>
+          <a 
+            href="https://www.linkedin.com/in/rajan-singh-5a9089295/" 
+            className="cta-btn secondary"
+            target="_blank" 
+            rel="noopener noreferrer"
+          >
+            Connect me
+          </a>
         </div>
       </div>
     </div>
